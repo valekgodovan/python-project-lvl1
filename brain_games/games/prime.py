7f8596ca907f78e1ef35b1b3a328f6e8ea7e1fa6
valek <valekgodovan@gmail.com>
2022-07-03 21:46:44 +0700

    add brain prime game

---
+++ brain_games/games/prime.py
@@ -0,0 +1,32 @@
+"""The brain prime logic."""
+import random
+
+from brain_games.game_logic import ask_question, hello_user, wrong_answer
+
+
+def is_prime(num):
+    for i in range(2, num + 1):
+        if num % i == 0:
+            div = i
+            break
+    if div == num:
+        return 'yes'
+    else:
+        return 'no'
+
+
+def prime():
+    name = hello_user()
+    step_counter = 0
+    while step_counter < 3:
+        num = random.randint(1, 20)
+        user_answer = ask_question(num)
+        correct_answer = is_prime(num)
+        if user_answer == correct_answer:
+            step_counter += 1
+            print('Correct!')
+        else:
+            wrong_answer(user_answer, correct_answer, name)
+            break
+    if step_counter == 3:
+        print(f'Congratulations, {name}!')
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""Main program."""
+from brain_games.games.prime import prime
+
+
+def main():
+    """Run great common divisor logic."""
+    prime()
+
+
+if __name__ == '__main__':
+    main()
