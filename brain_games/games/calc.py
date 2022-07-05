17f148ec12d25669276fd3e8c9c2e7975ae919c4
valek <valekgodovan@gmail.com>
2022-07-06 00:21:29 +0700

    add new calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,33 +1,25 @@
 """The calculator game logic."""
 import random
 
-from brain_games.game_logic import hello_user, wrong_answer
+from brain_games.game_logic import game_logic
+
+
+def give_task():
+    num_1 = random.randint(0, 10)
+    num_2 = random.randint(0, 10)
+    char = random.choice(['+', '-', '*'])
+    if char == '+':
+        task = f'{num_1} + {num_2}'
+        correct_answer = str(num_1 + num_2)
+    elif char == '-':
+        task =(f'{num_1} - {num_2}')
+        correct_answer = str(num_1 - num_2)
+    else:
+        task = f'{num_1} * {num_2}'
+        correct_answer = str(num_1 * num_2)
+    return (task, correct_answer)
 
 
 def calc():
-    name = hello_user()
-    print('What is the result of the expression?')
-    step_counter = 0
-    while step_counter < 3:
-        num_1 = random.randint(0, 10)
-        num_2 = random.randint(0, 10)
-        char = random.choice(['+', '-', '*'])
-        if char == '+':
-            print(f'Question: {num_1} + {num_2}')
-            correct_answer = str(num_1 + num_2)
-        elif char == '-':
-            print(f'Question: {num_1} - {num_2}')
-            correct_answer = str(num_1 - num_2)
-        else:
-            print(f'Question: {num_1} * {num_2}')
-            correct_answer = str(num_1 * num_2)
-        print('Your answer:', end=' ')
-        user_answer = input()
-        if user_answer == correct_answer:
-            step_counter += 1
-            print('Correct!')
-        else:
-            wrong_answer(user_answer, correct_answer, name)
-            break
-    if step_counter == 3:
-        print(f'Congratulations, {name}!')
+    question = 'What is the result of the expression?'
+    game_logic(question, give_task)    
