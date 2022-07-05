060f73d795d9ee3cb68cbb2735b97c7d6b50cd4b
valek <valekgodovan@gmail.com>
2022-07-06 00:18:59 +0700

    add new game logic

--- brain_games/game_logic.py
+++ brain_games/game_logic.py
@@ -2,36 +2,17 @@
 import prompt
 
 
-def hello_user():
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    return name
-
-
-def ask_question(task):
-    print(f'Question: {task}')
-    print('Your answer:', end=' ')
-    user_answer = input()
-    return user_answer
-
-
-def wrong_answer(user_answer, correct_answer, name):
-    print(f"'{user_answer}' is wrong answer ;(.", end=' ')
-    print(f"Correct answer was '{correct_answer}'.")
-    print(f"Let's try again, {name}!")
-
-
-def game_logic(question, task, correct_answer):
+def game_logic(question, func):
     print('Welcome to the Brain Games!')
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(question)
-    print(f'Question: {task}')
-    print('Your answer:', end=' ')
-    user_answer = input()
     step_counter = 0
     while step_counter < 3:
+        task, correct_answer = func()
+        print(f'Question: {task}')
+        print('Your answer:', end=' ')
+        user_answer = input()
         if user_answer == correct_answer:
             step_counter += 1
             print('Correct!')
@@ -39,5 +20,6 @@ def game_logic(question, task, correct_answer):
             print(f"'{user_answer}' is wrong answer ;(.", end=' ')
             print(f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
+            break
     if step_counter == 3:
         print(f'Congratulations, {name}!')
