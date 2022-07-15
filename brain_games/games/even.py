624f60a9e5cc338386aaed17f1c7a0536c2b05d6
valek <valekgodovan@gmail.com>
2022-07-16 01:26:50 +0700

    change brain-even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -7,8 +7,5 @@ QUESTUON = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 def get_task():
     task = random.randint(0, 99)
-    if task % 2 == 0:
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
-    return (task, correct_answer)
+    correct_answer = 'no' if task % 2 else 'yes'
+    return task, correct_answer
