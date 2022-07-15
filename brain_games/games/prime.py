e15ea6fb4db92f60e4bcfddeac70aedffafa764c
valek <valekgodovan@gmail.com>
2022-07-16 01:12:32 +0700

    change brain-prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,12 +7,12 @@ QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 def get_task():
     task = random.randint(0, 50)
-    divisor = 1
-    for i in range(2, task + 1):
+    divisor_counter = 0
+    for i in range(2, task // 2 + 1):
         if task % i == 0:
-            divisor = i
+            divisor_counter += 1
             break
-    if divisor == task:
+    if divisor_counter == 0:
         correct_answer = 'yes'
         return task, correct_answer
     else:
