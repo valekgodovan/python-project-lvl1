5a508b4fa80e723f216fb501d7e712e25305a000
valek <valekgodovan@gmail.com>
2022-07-16 15:03:14 +0700

    change brain-calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,5 +1,6 @@
 """The calculator game logic."""
 import random
+import operator
 
 
 QUESTION = 'What is the result of the expression?'
@@ -8,14 +9,12 @@ QUESTION = 'What is the result of the expression?'
 def get_task():
     num_1 = random.randint(0, 10)
     num_2 = random.randint(0, 10)
-    char = random.choice(['+', '-', '*'])
-    if char == '+':
-        task = f'{num_1} + {num_2}'
-        correct_answer = str(num_1 + num_2)
-    elif char == '-':
-        task = f'{num_1} - {num_2}'
-        correct_answer = str(num_1 - num_2)
-    else:
-        task = f'{num_1} * {num_2}'
-        correct_answer = str(num_1 * num_2)
+    operations = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul
+    }
+    operation = random.choice(list(operations.keys()))
+    correct_answer = str(operations[operation](num_1, num_2))
+    task = f'{num_1} {operation} {num_2}'
     return (task, correct_answer)
