b16621053f531477c047779fb424d88c5c1daa51
valek <valekgodovan@gmail.com>
2022-07-18 21:26:38 +0700

    fix brain-prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,7 +6,9 @@ QUESTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
 def is_prime(num):
-    if num == 1 or num == 2:
+    if num == 1:
+        return True
+    elif num == 2:
         return True
     if num % 2 == 0:
         return False
